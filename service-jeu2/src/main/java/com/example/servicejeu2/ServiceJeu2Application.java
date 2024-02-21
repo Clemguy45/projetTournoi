e9d5829fc3d6d101140e79d5ebc633784b034bbf
Daniel Adigoun <daniel.adigoun@etu.univ-orleans.fr>
2024-02-21 21:09:57 +0100

    Création de la branche et du module jeu2

---
+++ service-jeu2/src/main/java/com/example/servicejeu2/ServiceJeu2Application.java
@@ -0,0 +1,13 @@
+package com.example.servicejeu2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceJeu2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceJeu2Application.class, args);
+	}
+
+}
