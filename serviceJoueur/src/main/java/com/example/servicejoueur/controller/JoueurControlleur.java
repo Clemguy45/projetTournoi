0a949774b053fd0257c87696deab78deaa0ff2ed
Clemguy45 <clement.guyot@etu.univ-orleans.fr>
2024-02-23 21:54:31 +0100

    Mise en placed de la liaison gateway

--- serviceJoueur/src/main/java/com/example/servicejoueur/controller/JoueurControlleur.java
+++ serviceJoueur/src/main/java/com/example/servicejoueur/controller/JoueurControlleur.java
@@ -1,14 +1,16 @@
 package com.example.servicejoueur.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/joueur")
 public class JoueurControlleur {
+
     @GetMapping("/hello")
-    public String hello (){
-         return "ça marche";
+    public ResponseEntity hello() {
+        return ResponseEntity.ok().build();
     }
 }
