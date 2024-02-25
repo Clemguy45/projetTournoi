649ad3b81189fa114f0ab7fe605039eee9b360f2
Clemguy45 <clement.guyot@etu.univ-orleans.fr>
2024-02-25 22:08:00 +0100

    Mise en placed de la SecurityConfig avec Keycloak

--- serviceJoueur/src/main/java/com/example/servicejoueur/ServiceJoueurApplication.java
+++ serviceJoueur/src/main/java/com/example/servicejoueur/ServiceJoueurApplication.java
@@ -1,11 +1,11 @@
 package com.example.servicejoueur;
 
+import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
-@EnableWebFlux
 public class ServiceJoueurApplication {
 
     public static void main(String[] args) {
