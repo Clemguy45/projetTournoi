8d8fa7fb237dbfb0e9888419b1535ba163343125
Clemguy45 <clement.guyot@etu.univ-orleans.fr>
2024-03-04 11:57:04 +0100

    Debug Dockerfile et docker-compose.yml avec mise en place du consul

--- serviceJeu2/src/main/java/com/example/servicejeu2/ServiceJeu2Application.java
+++ serviceJeu2/src/main/java/com/example/servicejeu2/ServiceJeu2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServiceJeu2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceJeu2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceJeu2Application.class, args);
+    }
 
 }
