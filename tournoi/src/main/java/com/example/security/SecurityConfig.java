10985764c489d7c2657c262ca5f5cc7e320a698b
o22107014 <yohann.cholley@etu.univ-orleans.fr>
2024-04-06 19:09:27 +0200

    change de SecurityConfig.java

--- tournoi/src/main/java/com/example/security/SecurityConfig.java
+++ tournoi/src/main/java/com/example/security/SecurityConfig.java
@@ -1,21 +1,42 @@
 package com.example.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/tournoi/**").authenticated()
-                .anyRequest().permitAll()
-                .and()
-                .httpBasic();
+                .csrf(csrf ->csrf.disable())
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(HttpMethod.GET, "/actuator/health").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/accelerometre").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/informationsScore").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/distanceParcourue").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(conf -> conf.jwt(Customizer.withDefaults()))
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling((exceptions) -> exceptions
+                        .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
+                );
+        return http.build();
     }
+
 }
