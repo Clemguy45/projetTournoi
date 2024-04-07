ab7ba4465bf13941b699e8152b8302f74d5ba781
mickael <mickael.poudroux@etu.univ-orleans.fr>
2024-04-07 19:46:35 +0200

    docker build marche

---
+++ jeuxTri/src/main/java/com/example/jeuxtri/JeuxTriApplication.java
@@ -0,0 +1,13 @@
+package com.example.jeuxtri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeuxTriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeuxTriApplication.class, args);
+	}
+
+}
---
+++ jeuxTri/src/test/java/com/example/jeuxtri/JeuxTriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jeuxtri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeuxTriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
