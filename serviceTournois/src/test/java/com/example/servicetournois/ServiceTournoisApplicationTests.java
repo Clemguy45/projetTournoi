8194be10bb7af6215f5daa1bbb0049cdc7cf4ec8
Clement <o2163453@etu.univ-orleans.fr>
2024-04-12 22:43:08 +0200

    Mise en place de la BDD et refonte de Tournois

---
+++ serviceTournois/src/test/java/com/example/servicetournois/ServiceTournoisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicetournois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTournoisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
